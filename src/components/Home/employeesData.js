c1d3670ea6806343202b2b3bd11d56a8972e5885
Jakub Cigánek <ciganekkuba@gmail.com>
2023-01-13 14:38:18 +0100

    ReactCvikoApp

---
+++ src/components/Home/employeesData.js
@@ -0,0 +1,28 @@
+/* eslint-disable */
+export const employees = [
+  {
+    id: 1,
+    name: "Jakub Cigánek",
+    sex: "muž",
+  },
+  {
+    id: 2,
+    name: "Tomáš Zádrapa",
+    sex: "muž",
+  },
+  {
+    id: 3,
+    name: "Lucie Jochmanová",
+    sex: "žena",
+  },
+  {
+    id: 4,
+    name: "Barbora Cigánková",
+    sex: "žena",
+  },
+  {
+    id: 5,
+    name: "Marina Pěntáková",
+    sex: "žena",
+  },
+];
